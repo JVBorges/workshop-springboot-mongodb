b300c666a5f5f887412f176dcb394086fd4bd77e
João Vitor Borges <joao-salves@hotmail.com>
2019-11-13 00:58:04 -0300

    query methods

--- src/main/java/com/jvborges/wsmongodb/repository/PostRepository.java
+++ src/main/java/com/jvborges/wsmongodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.jvborges.wsmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.jvborges.wsmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
