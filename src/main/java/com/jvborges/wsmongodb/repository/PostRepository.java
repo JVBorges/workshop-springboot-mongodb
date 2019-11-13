dc5ce4b4d7b43acd77a33e6d30180223e1b7ee94
João Vitor Borges <joao-salves@hotmail.com>
2019-11-13 01:03:46 -0300

    @Query

--- src/main/java/com/jvborges/wsmongodb/repository/PostRepository.java
+++ src/main/java/com/jvborges/wsmongodb/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.jvborges.wsmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jvborges.wsmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
