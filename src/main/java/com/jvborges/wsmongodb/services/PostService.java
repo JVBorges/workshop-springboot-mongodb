4e524ce24b5f317ee01b09213c193ca5c093a590
João Vitor Borges <joao-salves@hotmail.com>
2019-11-13 00:38:09 -0300

    Post findById

---
+++ src/main/java/com/jvborges/wsmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.jvborges.wsmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jvborges.wsmongodb.domain.Post;
+import com.jvborges.wsmongodb.repository.PostRepository;
+import com.jvborges.wsmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+		
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+}
