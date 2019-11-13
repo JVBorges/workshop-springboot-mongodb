5f7293828c005f2d62b2dff25c6b0bbcd11b96d4
João Vitor Borges <joao-salves@hotmail.com>
2019-11-13 00:24:09 -0300

    AuthorDTO

--- src/main/java/com/jvborges/wsmongodb/config/Instantiation.java
+++ src/main/java/com/jvborges/wsmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.jvborges.wsmongodb.domain.Post;
 import com.jvborges.wsmongodb.domain.User;
+import com.jvborges.wsmongodb.dto.AuthorDTO;
 import com.jvborges.wsmongodb.repository.PostRepository;
 import com.jvborges.wsmongodb.repository.UserRepository;
 
@@ -33,10 +34,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2019"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepo.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2019"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepo.saveAll(Arrays.asList(post1, post2));
 	}
 
